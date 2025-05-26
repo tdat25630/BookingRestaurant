f8f4fc3337cdbb7c4f3655984584858c1232d5e8
trantdat <dattthe172813@fpt.edu.vn>
2025-05-26 13:39:16 +0700

    init project

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+require('dotenv').config();
+
+const connectDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+
+    console.error("MongoDB connection failed: ", error);
+    process.exit(1);
+  }
+}
+
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack
+  })
+})
+
+app.listen(8989, () => {
+  connectDb();
+  console.log("Server is running on port 8989");
+})
